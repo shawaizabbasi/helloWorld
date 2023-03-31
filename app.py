375bfcbe53648b02ef0e383555607ef748554f5a
Muhammad Abbasi <mabbasi@terpmail.umd.edu>
2023-03-31 00:00:43 -0400

    Added additional touches, the base html and completed requirements.

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World from Shawaiz Abbasi! I am adding my first code change.'
 
+@app.route('/favorite-course')
+def favorite():
+    return render_template('favorite-course.html')
+
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
@@ -19,16 +23,13 @@ def hello():
     return render_template('hello.html')
 
 
-@app.route('/favorite-course')
-def favorite():
-    return render_template('favorite-course.html')
-
-
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-
+@app.route('/base')
+def base():
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
