cfb296b196f9815c19bea8a2c1806bc512a2614f
Muhammad Abbasi <mabbasi@terpmail.umd.edu>
2023-03-30 20:18:45 -0400

    Created route configurations, the contact html and the favorite-course html with the correct display following a bootstrap style.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,13 +7,29 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World from Shawaiz Abbasi! I am adding my first code change.'
 
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
 
+
+@app.route('/favorite-course')
+def favorite():
+    return render_template('favorite-course.html')
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+
 
 if __name__ == '__main__':
     app.run()
